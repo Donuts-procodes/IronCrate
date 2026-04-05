5381c0c93ab0bc1241d706b8238f5cb6ef0398bf
Donuts-procodes <donut.programs1010@gmail.com>
2026-04-05 22:37:04 +0530

    changes in frontenf

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,13 @@
+// src/api.js
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  // Updated with your active localtunnel URL
+  baseURL: 'https://common-dolls-cough.loca.lt/api', 
   withCredentials: true,
+  headers: {
+    "Bypass-Tunnel-Reminder": "true" 
+  }
 })
 
 export default api
